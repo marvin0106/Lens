3d8eed3e1a3fe7c0d274a2f01be1e1a554190a3f
marvin0106 <123030031+marvin0106@users.noreply.github.com>
2025-03-25 17:07:15 +0000

    Basics integriert

---
+++ 0_HOME.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("My new Lens App")
+st.write(
+    "Ein kleines Sommerprojekt aus Lust und Laune")
+tab1, tab2, tab3 = st.tabs(["Willkommen", "Todo", "LEER"])
+with tab1: # Welcome Page
+    'Dies ist mein kleines Privates Projekt, hier möchte ich ein paar Stunden investieren.'
+    'Ich möchte eine App zu kreieren, welche Short-Term und Long-Term Investment-Entscheidungen unterstützen soll.'
+with tab2: # Todo-Liste
+    'Was noch Ansteht:'
+    '- Longterm-Tab: Insider Scraping integrieren. Möglichst viel aus altem Projekt übernehmen.'
+    '- Shortterm-Tab: Integration von Fundamentalanalyse und Indikatoren. Evtl. Option Pricing als Tab hierunter einführen'
+    '- Integration vom Investment-Rechner den ich bereits in Excel umgesetzt hatte. -> Evtl. Python in Excel navigieren lassen.'
+with st.sidebar: st.write('Notes in Sidebar')
+with tab3:
+    'LEERER TAB'
