cdb28576243d896d8955969986b12f45b1c88be5
marvin0106 <123030031+marvin0106@users.noreply.github.com>
2025-03-25 18:11:26 +0000

    Notetaking & Completion for the day

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ with tab2: # Todo-Liste
     '- Longterm-Tab: Insider Scraping integrieren. Möglichst viel aus altem Projekt übernehmen.'
     '- Shortterm-Tab: Integration von Fundamentalanalyse und Indikatoren. Evtl. Option Pricing als Tab hierunter einführen'
     '- Integration vom Investment-Rechner den ich bereits in Excel umgesetzt hatte. -> Evtl. Python in Excel navigieren lassen.'
+    '- Fear and Greed Index wie bei Quiverquant nur evtl. stattdessen durch Twitter Scraping'
+    'Lobbying index mit api auf https://lda.senate.gov/api/ wie https://www.quiverquant.com/lobbying/'
 with st.sidebar: st.write('Notes in Sidebar')
 with tab3:
     'LEERER TAB'
